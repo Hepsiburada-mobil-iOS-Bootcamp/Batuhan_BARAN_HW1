e1e8e3050aedb78bb2e07cb1f3aeb63b43d090c2
batuhanbaran <fb_batuhan_baran@hotmail.com>
2021-09-22 21:01:44 +0300

    mergeArray added.

--- iOS_Week_1/Managers/AlgorithmManager.swift
+++ iOS_Week_1/Managers/AlgorithmManager.swift
@@ -96,7 +96,10 @@ class AlgoruthmManager: AlgorithmProtocol {
     
      You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
      Merge nums1 and nums2 into a single array sorted in non-decreasing order.
-     The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+     
+     The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+     
+     To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
      
      Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
      Output: [1,2,2,3,5,6]
@@ -104,11 +107,17 @@ class AlgoruthmManager: AlgorithmProtocol {
      The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
      */
     func mergeArraysTest() {
-        
+        var nums1 = [1,2,3,0,0,0]
+        let nums2 = [2,5,6,0,0]
+        merge(&nums1, nums1.count, nums2, nums2.count)
     }
     
     private func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-            
+        for num2 in nums2 {
+            nums1.append(num2)
+        }
+        nums1 = nums1.filter({ $0 != 0 })
+        print(nums1.sorted())
     }
     
     // MARK: - Intersection of Two Arrays
@@ -119,12 +128,12 @@ class AlgoruthmManager: AlgorithmProtocol {
      Output: [2,2]
      */
     func arrayIntersectionTest() {
-        
+        print(intersect([1,2,2,1], [2,2]))
     }
     
-//    func intersect(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
-//
-//    }
+    func intersect(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+        return nums2.filter({nums1.contains($0)})
+    }
     
     // MARK: - Missing Number
     /*
